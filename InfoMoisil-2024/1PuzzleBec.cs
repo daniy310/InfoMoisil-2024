8fde935fc29893a465e304ce0fbbc56b9f904613
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-01-26 11:57:28 +0200

    Update 1PuzzleBec.cs

--- InfoMoisil-2024/1PuzzleBec.cs
+++ InfoMoisil-2024/1PuzzleBec.cs
@@ -12,9 +12,22 @@ namespace InfoMoisil_2024
 {
     public partial class _1PuzzleBec : Form
     {
+        bool[] buttonsState = new bool[10];
+
         public _1PuzzleBec()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) { buttonsState[0] = !buttonsState[0]; }
+        private void button2_Click(object sender, EventArgs e) { buttonsState[1] = !buttonsState[1]; }
+        private void button3_Click(object sender, EventArgs e) { buttonsState[2] = !buttonsState[2]; }
+        private void button4_Click(object sender, EventArgs e) { buttonsState[3] = !buttonsState[3]; }
+        private void button5_Click(object sender, EventArgs e) { buttonsState[4] = !buttonsState[4]; }
+        private void button6_Click(object sender, EventArgs e) { buttonsState[5] = !buttonsState[5]; }
+        private void button7_Click(object sender, EventArgs e) { buttonsState[6] = !buttonsState[6]; }
+        private void button8_Click(object sender, EventArgs e) { buttonsState[7] = !buttonsState[7]; }
+        private void button9_Click(object sender, EventArgs e) { buttonsState[8] = !buttonsState[8]; }
+        private void button10_Click(object sender, EventArgs e) { buttonsState[9] = !buttonsState[9]; }
     }
 }
