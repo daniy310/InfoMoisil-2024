ea5f07f5a61c43e9d660e2bc5792d79801e7bc6e
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-01-26 12:38:16 +0200

    Validation System

--- InfoMoisil-2024/1PuzzleBec.Designer.cs
+++ InfoMoisil-2024/1PuzzleBec.Designer.cs
@@ -27,138 +27,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
-            this.checkBox6 = new System.Windows.Forms.CheckBox();
-            this.checkBox7 = new System.Windows.Forms.CheckBox();
-            this.checkBox8 = new System.Windows.Forms.CheckBox();
-            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_1PuzzleBec));
+            this.buttonsPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.buttonsPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // buttonsPanel
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(14, 346);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.buttonsPanel.Controls.Add(this.button1);
+            this.buttonsPanel.Controls.Add(this.button2);
+            this.buttonsPanel.Controls.Add(this.button3);
+            this.buttonsPanel.Controls.Add(this.button4);
+            this.buttonsPanel.Controls.Add(this.button5);
+            this.buttonsPanel.Controls.Add(this.button6);
+            this.buttonsPanel.Controls.Add(this.button7);
+            this.buttonsPanel.Controls.Add(this.button8);
+            this.buttonsPanel.Controls.Add(this.button9);
+            this.buttonsPanel.Controls.Add(this.button10);
+            this.buttonsPanel.Location = new System.Drawing.Point(40, 338);
+            this.buttonsPanel.Name = "buttonsPanel";
+            this.buttonsPanel.Size = new System.Drawing.Size(733, 100);
+            this.buttonsPanel.TabIndex = 0;
             // 
-            // checkBox2
+            // button1
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(100, 346);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // checkBox3
+            // button2
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(186, 346);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(80, 17);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "checkBox3";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(76, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 82);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
-            // checkBox4
+            // button3
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(272, 346);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(80, 17);
-            this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "checkBox4";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(149, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 82);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
-            // checkBox5
+            // button4
             // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(616, 346);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(80, 17);
-            this.checkBox5.TabIndex = 7;
-            this.checkBox5.Text = "checkBox5";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.button4.Location = new System.Drawing.Point(222, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 82);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
             // 
-            // checkBox6
+            // button5
             // 
-            this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(530, 346);
-            this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(80, 17);
-            this.checkBox6.TabIndex = 6;
-            this.checkBox6.Text = "checkBox6";
-            this.checkBox6.UseVisualStyleBackColor = true;
+            this.button5.Location = new System.Drawing.Point(295, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(67, 82);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
             // 
-            // checkBox7
+            // button6
             // 
-            this.checkBox7.AutoSize = true;
-            this.checkBox7.Location = new System.Drawing.Point(444, 346);
-            this.checkBox7.Name = "checkBox7";
-            this.checkBox7.Size = new System.Drawing.Size(80, 17);
-            this.checkBox7.TabIndex = 5;
-            this.checkBox7.Text = "checkBox7";
-            this.checkBox7.UseVisualStyleBackColor = true;
+            this.button6.Location = new System.Drawing.Point(368, 3);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(67, 82);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
             // 
-            // checkBox8
+            // button7
             // 
-            this.checkBox8.AutoSize = true;
-            this.checkBox8.Location = new System.Drawing.Point(358, 346);
-            this.checkBox8.Name = "checkBox8";
-            this.checkBox8.Size = new System.Drawing.Size(80, 17);
-            this.checkBox8.TabIndex = 4;
-            this.checkBox8.Text = "checkBox8";
-            this.checkBox8.UseVisualStyleBackColor = true;
+            this.button7.Location = new System.Drawing.Point(441, 3);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(67, 82);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
             // 
-            // checkBox9
+            // button8
             // 
-            this.checkBox9.AutoSize = true;
-            this.checkBox9.Location = new System.Drawing.Point(702, 346);
-            this.checkBox9.Name = "checkBox9";
-            this.checkBox9.Size = new System.Drawing.Size(80, 17);
-            this.checkBox9.TabIndex = 8;
-            this.checkBox9.Text = "checkBox9";
-            this.checkBox9.UseVisualStyleBackColor = true;
+            this.button8.Location = new System.Drawing.Point(514, 3);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(67, 82);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(587, 3);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(67, 82);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click_1);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(660, 3);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(67, 82);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click_1);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(554, 191);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(213, 66);
+            this.button11.TabIndex = 1;
+            this.button11.Text = "VALIDATE";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // _1PuzzleBec
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox9);
-            this.Controls.Add(this.checkBox5);
-            this.Controls.Add(this.checkBox6);
-            this.Controls.Add(this.checkBox7);
-            this.Controls.Add(this.checkBox8);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.buttonsPanel);
             this.Name = "_1PuzzleBec";
             this.Text = "_1PuzzleBec";
+            this.buttonsPanel.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
-        private System.Windows.Forms.CheckBox checkBox5;
-        private System.Windows.Forms.CheckBox checkBox6;
-        private System.Windows.Forms.CheckBox checkBox7;
-        private System.Windows.Forms.CheckBox checkBox8;
-        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.FlowLayoutPanel buttonsPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
     }
 }
--- InfoMoisil-2024/1PuzzleBec.cs
+++ InfoMoisil-2024/1PuzzleBec.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,118 @@ namespace InfoMoisil_2024
 {
     public partial class _1PuzzleBec : Form
     {
+        bool[] secv = new bool[10] {
+            true,
+            false,
+            false,
+            true,
+            true,
+            false,
+            false,
+            true,
+            false,
+            true,
+        };
+
         bool[] buttonsState = new bool[10];
+
+        static Image LoadImage(String name)
+        {
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            string offFile = System.IO.Path.Combine(sCurrentDirectory, $@"..\..\..\Assets\{name}.png");
+            string offFilePath = Path.GetFullPath(offFile);
+            Image image = Image.FromFile(offFilePath);
+
+            return image;
+        }
+
+        Image onImage = LoadImage("BulbOn");
+        Image offImage = LoadImage("BulbOff");
+
+        void UpdateButtonVisuals()
+        {
+            int i = 0;
+            foreach (Button button in buttonsPanel.Controls.OfType<Button>()) {
+                button.BackgroundImage = buttonsState[i] ? onImage : offImage;
+                button.BackgroundImageLayout = ImageLayout.Stretch;
+                i++;
+            }
+        }
 
         public _1PuzzleBec()
         {
             InitializeComponent();
+
+            UpdateButtonVisuals();
         }
 
-        private void button1_Click(object sender, EventArgs e) { buttonsState[0] = !buttonsState[0]; }
-        private void button2_Click(object sender, EventArgs e) { buttonsState[1] = !buttonsState[1]; }
-        private void button3_Click(object sender, EventArgs e) { buttonsState[2] = !buttonsState[2]; }
-        private void button4_Click(object sender, EventArgs e) { buttonsState[3] = !buttonsState[3]; }
-        private void button5_Click(object sender, EventArgs e) { buttonsState[4] = !buttonsState[4]; }
-        private void button6_Click(object sender, EventArgs e) { buttonsState[5] = !buttonsState[5]; }
-        private void button7_Click(object sender, EventArgs e) { buttonsState[6] = !buttonsState[6]; }
-        private void button8_Click(object sender, EventArgs e) { buttonsState[7] = !buttonsState[7]; }
-        private void button9_Click(object sender, EventArgs e) { buttonsState[8] = !buttonsState[8]; }
-        private void button10_Click(object sender, EventArgs e) { buttonsState[9] = !buttonsState[9]; }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[0] = !buttonsState[0]; UpdateButtonVisuals();
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[1] = !buttonsState[1]; UpdateButtonVisuals();
+
+        }
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[2] = !buttonsState[2]; UpdateButtonVisuals();
+
+        }
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[3] = !buttonsState[3]; UpdateButtonVisuals();
+
+        }
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[4] = !buttonsState[4]; UpdateButtonVisuals();
+
+        }
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[5] = !buttonsState[5]; UpdateButtonVisuals();
+
+        }
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[6] = !buttonsState[6]; UpdateButtonVisuals();
+
+        }
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[7] = !buttonsState[7]; UpdateButtonVisuals();
+
+        }
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[8] = !buttonsState[8]; UpdateButtonVisuals();
+
+        }
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            buttonsState[9] = !buttonsState[9]; UpdateButtonVisuals();
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            // Validate
+            if (IsValid()) {
+                MessageBox.Show("CORRECT");
+            } else {
+                MessageBox.Show("Try Again");
+            }
+        }
+
+        bool IsValid()
+        {
+            for (int i = 0; i < 10; i++) {
+                if (buttonsState[i] != secv[i]) return false;
+            }
+            return true;
+        }
     }
 }
