f80389281a29855d5932a873d4964b32c92be2b5
M3Darius <mdddarius@gmail.com>
2024-01-26 13:15:50 +0200

    Added helperman1, helperman2

---
+++ InfoMoisil-2024/HelperMan.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfoMoisil_2024
+{
+	public partial class HelperMan : Form
+	{
+		Random r = new Random(3);
+		char[] chars = "ඉඊඏගඞජටණනළ෦෧෬෭෮෴".ToCharArray();
+		char[] indicators = "⚪⚫".ToCharArray();
+
+		Label[] labels;
+		public HelperMan()
+		{
+			InitializeComponent();
+			labels = new Label[16];
+			for(int i = 0; i < 16; i++)
+			{
+				var v = r.Next(0,4);
+				string str = "";
+				switch (v)
+				{
+					case 0:
+						str += "⚪⚪";
+						break;
+					case 1:
+						str += "⚪⚫";
+						break;
+					case 2:
+						str += "⚫⚪";
+						break;
+					case 3:
+						str += "⚫⚫";
+						break;
+					default:
+						break;
+				}
+				str += chars[i];
+				labels[i] = new Label();
+				labels[i].Text = str;
+				labels[i].AutoSize = true;
+				//labels[i].Location = new Point(i / 4 * 119, (i%4) * 37);
+				labels[i].Font = new Font("Microsoft Sans Serif", 24);
+				labels[i].Dock = DockStyle.Fill;
+				tableLayoutPanel1.Controls.Add(labels[i], i / 4, i % 4);
+			}
+
+		}
+
+		private void labelCodeTemplate_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
